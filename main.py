d450453f9672ee2ba6012687789ebf59d4ad0ed7
BugSpace <409478@niuitmo.ru>
2026-01-18 18:25:21 +0700

    add square

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import pygame
 
 pygame.init()
-screen = pygame.display.set_mode((600, 600))
+screen_width = 600
+screen = pygame.display.set_mode((screen_width, screen_width))
 pygame.display.set_caption("Вверх по лестнице")
+
+square = pygame.Surface((screen_width/20, screen_width/20))
+square.fill('white')
 
 running = True
 while running:
+    screen.blit(square, (0, 0))
+
     pygame.display.update()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
