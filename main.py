5612b5109be0bff04a9bb22bd19409ca0935fb13
BugSpace <409478@niuitmo.ru>
2026-01-18 18:47:14 +0700

    add circle

--- main.py
+++ main.py
@@ -5,15 +5,17 @@ screen_width = 600
 screen = pygame.display.set_mode((screen_width, screen_width))
 pygame.display.set_caption("Вверх по лестнице")
 
-square = pygame.Surface((screen_width/20, screen_width/20))
-square.fill('white')
-
 running = True
+player_coord = (screen_width/40, screen_width * 19 / 20 - screen_width/40)
 while running:
-    for i in range(20):
-        coord = (screen_width*i/20, screen_width*(20-i-1)/20)
-        screen.blit(square, coord)
+    screen.fill('black')
+    square = pygame.Surface((screen_width / 20, screen_width / 20))
+    square.fill('white')
 
+    for i in range(20):
+        coord = (screen_width * i / 20, screen_width * (20 - i - 1) / 20)
+        screen.blit(square, coord)
+    pygame.draw.circle(screen, "red", (player_coord[0], player_coord[1]), screen_width/40)
     pygame.display.update()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
